c72b9f3bb917f65ea031242396f4c6c38fb6979c
Durvesh Wagh <durveshwagh11@gmail.com>
2024-06-29 09:53:58 +0530

    Push

--- Basics/Java6.java
+++ Basics/Java6.java
@@ -1,2 +1,17 @@
+//Find the Greatest of the Three Numbers in Java
+
+import java.util.Scanner;
+
 public class Java6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int temp, result;
+        temp = Math.max(a, b);
+        result = Math.max(c, temp);
+        System.out.println(result);
+    }
 }
--- Basics/Java7.java
+++ Basics/Java7.java
@@ -1,2 +1,36 @@
+//Check Whether the Year is a Leap Year in Java
+import java.util.Scanner;
+
 public class Java7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+
+        if (year % 400 == 0)
+            System.out.println (year + " is a Leap Year");
+        else if (year % 4 == 0 && year % 100 != 0)
+            System.out.println (year + " is a Leap Year");
+        else
+            System.out.println (year + " is not a Leap Year");
+    }
 }
+
+
+
+//Using Ternary Operator
+//
+//    public static void main (String[]args)
+//    {
+//
+//        int year = 2019;
+//
+//        int flag = (year%400 == 0) || (year%4==0 && year%100!=0 ) ? 1 : 0;
+//        if (flag ==1)
+//        {
+//            System.out.println (year + " is a Leap Year");
+//        }
+//        else
+//        {
+//            System.out.println (year + " is not a Leap Year");
+//        }
+//    }
